ceb16cf0f83c57c58bba35ab2040e8f7e9886f3b
wahl <giovaniwahl@gmail.com>
2023-11-15 17:28:39 -0300

    Game sid

--- src/main/java/com/giovaniwahl/dslist/domain/entities/Game.java
+++ src/main/java/com/giovaniwahl/dslist/domain/entities/Game.java
@@ -14,35 +14,39 @@ public class Game implements Serializable {
     private static final long serialVersionUID= 1L;
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private UUID id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private String title;
     @Column(name = "game_year")
     private String year;
     private String genre;
-    private String platform;
+    private String platforms;
+    private Double score;
     private String imgUrl;
+    @Column(columnDefinition = "TEXT")
     private String shortDescription;
+    @Column(columnDefinition = "TEXT")
     private String longDescription;
 
     public Game(){}
 
-    public Game(UUID id, String title, String year, String genre, String platform, String imgUrl,
+    public Game(Long id, String title, String year, String genre, String platforms,Double score, String imgUrl,
                 String shortDescription, String longDescription) {
         this.id = id;
         this.title = title;
         this.year = year;
         this.genre = genre;
-        this.platform = platform;
+        this.platforms = platforms;
+        this.score = score;
         this.imgUrl = imgUrl;
         this.shortDescription = shortDescription;
         this.longDescription = longDescription;
     }
 
-    public UUID getId() {
+    public Long getId() {
         return id;
     }
-    public void setId(UUID id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
@@ -67,11 +71,18 @@ public class Game implements Serializable {
         this.genre = genre;
     }
 
-    public String getPlatform() {
-        return platform;
+    public String getPlatforms() {
+        return platforms;
     }
-    public void setPlatform(String platform) {
-        this.platform = platform;
+    public void setPlatforms(String platforms) {
+        this.platforms = platforms;
+    }
+
+    public Double getScore() {
+        return score;
+    }
+    public void setScore(Double score) {
+        this.score = score;
     }
 
     public String getImgUrl() {
