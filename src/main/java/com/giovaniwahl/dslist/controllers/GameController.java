51ff2340763450790b320aca7509251c8beed269
wahl <giovaniwahl@gmail.com>
2023-11-16 15:54:14 -0300

    ajuste game service Impl

--- src/main/java/com/giovaniwahl/dslist/controllers/GameController.java
+++ src/main/java/com/giovaniwahl/dslist/controllers/GameController.java
@@ -1,19 +1,17 @@
 package com.giovaniwahl.dslist.controllers;
 
 import com.giovaniwahl.dslist.domain.dtos.GameShortDto;
-import com.giovaniwahl.dslist.domain.entities.Game;
 import com.giovaniwahl.dslist.domain.services.game.GameService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/games")
 public class GameController {
-    private GameService gameService;
+    private final GameService gameService;
     public GameController(GameService gameService) {
         this.gameService = gameService;
     }
