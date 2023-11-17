619db79d07f5077a7f3a17015862425b3080bbf3
wahl <giovaniwahl@gmail.com>
2023-11-17 20:04:47 -0300

    Deply is ready

--- src/main/java/com/giovaniwahl/dslist/domain/services/game/GameServiceImpl.java
+++ src/main/java/com/giovaniwahl/dslist/domain/services/game/GameServiceImpl.java
@@ -13,8 +13,8 @@ import java.util.List;
 
 @Service
 public class GameServiceImpl implements GameService{
-    @Autowired
     private final GameRepository gameRepository;
+    @Autowired
     public GameServiceImpl(GameRepository gameRepository) {
         this.gameRepository = gameRepository;
     }
